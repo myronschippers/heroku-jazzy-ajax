3040200f382f097bfcf584a8162f8f0e886f85ff
Myron <dubtwee@gmail.com>
2020-07-08 11:50:32 -0500

    sweet heroku config

--- server/routes/song.router.js
+++ server/routes/song.router.js
@@ -1,17 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const pg = require('pg');
+const url = require('url');
 
 const Pool = pg.Pool; // Class
 
+let config = {};
+
+if (process.env.DATABASE_URL) {
+
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true, // heroku requires ssl to be true
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+} else {
+  config = {
+    database: 'jazzy_ajax', // name of our database
+    host: 'localhost', // where is your database?
+    port: 5432, // this is the default port
+    max: 10, // number of connections
+    idleTimeoutMillis: 10000, // 10 seconds
+  };
+}
+
 // Connect Node to our database
-const pool = new Pool({
-  database: 'jazzy_ajax', // name of our database
-  host: 'localhost', // where is your database?
-  port: 5432, // this is the default port
-  max: 10, // number of connections
-  idleTimeoutMillis: 10000, // 10 seconds
-});
+const pool = new Pool(config);
 
 //ROUTES
 router.post('/', (req, res) => {
